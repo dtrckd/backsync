6ab8ea77a7bfc283d5a603d801a98d9e7430c215
dtrckd <ddtracked@gmail.com>
2016-11-05 19:33:47 +0100

    arg v0

--- backsync.sh
+++ backsync.sh
@@ -1,20 +1,34 @@
 #!/bin/bash
 
 #SSH='dtrckd@lala'
+D="Papiers"
+last_arg="${@: -1}"
 
-IN="./"
-OUT="/media/joker/TOSHIBA EXT/SC/Projets/"
+if [ -d "$last_arg" ]; then
+    D="$last_arg"
+fi
+
+IN="./$D/"
+OUT="/media/joker/TOSHIBA EXT/SC/$D/"
 
 SIMUL="-n"
-OPTS="--update --no-o --no-g --no-p  --modify-window=1"
+OPTS="--update --no-o --no-g --no-p  --modify-window=1 --iconv=iso-8859-1,utf8"
 OPTS="$OPTS --delete-after -O"
 
 if [ "$1" == "-f" ]; then
-    SIMUL=""; fi
+    SIMUL=""
+elif [ "$1" == "-h" ]; then
+    echo "backsync [-f (force)] [Target Directory ($D]"
+    exit 1
+fi
+    
 
 #rsync $SIMUL  -av -u --modify-window=2 --stats -m $OPTS \
 rsync $SIMUL -avh  $OPTS --stats -m -s \
     $IN "$OUT"
+
+echo "rsync $SIMUL -avh  $OPTS --stats -m -s \
+         $IN "$OUT""
 
 
 ###
